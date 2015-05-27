94bb2c43b3215b8220ffc3eedebbde06b5edf135
jcuellar1 <jcuellar@kippnyccp.org>
2015-05-27 09:40:52 -0400

    Update obstacle-ovoiding.py

--- obstacle-ovoiding.py
+++ obstacle-ovoiding.py
@@ -5,10 +5,11 @@ def move_left():
   enc_tgt(0,1,14)
   time.sleep(.1)
   left()
-  time.sleep(4)
-while us_dist(15) >25:
-  fwd():
-stop(): 
-move_left():
-while us_dist(15) >25:
-  fwd():
+  time.sleep(2)
+  
+while 1<2:
+  while us_dist(15) >25:
+  fwd()
+stop()
+move_left()
+
