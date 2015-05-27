644c964ab147311f048386f30d96a105e83b21f5
jcuellar1 <jcuellar@kippnyccp.org>
2015-05-27 09:27:56 -0400

    Create obstacle-ovoiding.py

---
+++ obstacle-ovoiding.py
@@ -0,0 +1,12 @@
+from gopigo import *
+set_speed (100):
+led_on()
+def move_left():
+  enc_tgt(0,1,14)
+  time.sleep(4)
+while us_dist(15) >25:
+  fwd():
+stop(): 
+move_left():
+while us_dist(15) >25:
+  fwd():
